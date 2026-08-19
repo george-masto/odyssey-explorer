fecdaefdf3772f6d8b9522f8d4401b593972f73d
george-masto <georgemasto@gmail.com>
2026-08-19 14:13:13 -0700

    Favicon: the trireme on a parchment roundel (SVG + PNG + apple-touch); drop scaffold icon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,14 @@
 <script lang="ts">
+	import { base } from '$app/paths';
 	import '../app.css';
 
 	let { children } = $props();
 </script>
+
+<svelte:head>
+	<link rel="icon" href="{base}/favicon.svg" type="image/svg+xml" />
+	<link rel="icon" href="{base}/favicon.png" type="image/png" sizes="64x64" />
+	<link rel="apple-touch-icon" href="{base}/apple-touch-icon.png" />
+</svelte:head>
 
 {@render children()}
